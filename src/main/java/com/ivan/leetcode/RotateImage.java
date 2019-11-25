b09407432ebe686eaea9e9ce0c2e3180c8dde4c5
qinge <qinge@tongbanjie.com>
2019-11-25 14:17:23 +0800

    48. 旋转图像

--- src/main/java/com/ivan/leetcode/RotateImage.java
+++ src/main/java/com/ivan/leetcode/RotateImage.java
@@ -52,7 +52,58 @@ public class RotateImage {
     @Test
     public void test(){
         int[][] matrix=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        showMatrix(matrix);
         rotate(matrix);
+        showMatrix(matrix);
+//        Assert.assertArrayEquals(new int[]{7,4,1},matrix[0]);
+//        Assert.assertArrayEquals(new int[]{8,5,2},matrix[1]);
+//        Assert.assertArrayEquals(new int[]{9,6,3},matrix[2]);
+    }
+
+    public void test2(){
+        int[] nums=new int[]{1,2,3,4,5,6};
+        for(int i=0;i<=nums.length/2;i++){
+            int temp=nums[i];
+        }
+    }
+
+    //单次循环旋转  对于单次任务点(i,j),四边形旋转四次换位：(i,j)-->(j,n-1-i)-->(n-1-i,n-1-j)-->(n-1-j,i)-->(i,j)
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        for(int i=0;i<(n+1)/2;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[n-1-j][i];
+                matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
+                matrix[j][n-1-i]=temp;
+            }
+        }
+    }
+
+    //先转置矩阵，然后翻转每一行 复杂度 ON2
+    public void rotate2(int[][] matrix) {
+        //rotage matrix
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i;j<matrix[i].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+            showMatrix(matrix);
+        }
+        showMatrix(matrix);
+        //reverse each row
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<=matrix[i].length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix[i].length-1-i];
+                matrix[i][matrix[i].length-1-i]=temp;
+            }
+        }
+    }
+
+    private void showMatrix(int[][] matrix){
         for(int[] a:matrix){
             for(int i:a){
                 System.out.print(i+",");
@@ -60,26 +111,6 @@ public class RotateImage {
             System.out.println();
 
         }
-//        Assert.assertArrayEquals(new int[]{7,4,1},matrix[0]);
-//        Assert.assertArrayEquals(new int[]{8,5,2},matrix[1]);
-//        Assert.assertArrayEquals(new int[]{9,6,3},matrix[2]);
-    }
-
-    public void rotate(int[][] matrix) {
-        int i=0;//行
-        int j=0;//列
-        int n=matrix.length-1;
-        int temp=matrix[j][n-i];
-        matrix[j][n-i]=matrix[i][j];
-        matrix[0][0]=temp;
-        i=n-j;
-        j=n-i;
-        while (i!=0 || j!=0){
-            temp=matrix[j][n-i];
-            matrix[j][n-i]=matrix[i][j];
-            matrix[0][0]=temp;
-            i=j;
-            j=n-i;
-        }
+        System.out.println("-----------------");
     }
 }
