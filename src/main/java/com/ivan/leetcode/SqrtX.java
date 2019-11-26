37cd6d7962cf9f5d68af8c28936d0a291f3114fd
qinge <qinge@tongbanjie.com>
2019-11-26 10:47:19 +0800

    69. x 的平方根

---
+++ src/main/java/com/ivan/leetcode/SqrtX.java
@@ -0,0 +1,51 @@
+package com.ivan.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 69. x 的平方根
+ *
+ * 实现 int sqrt(int x) 函数。
+ *
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ *
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ *
+ * 示例 1:
+ *
+ * 输入: 4
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842...,
+ *      由于返回类型是整数，小数部分将被舍去。
+ * */
+public class SqrtX {
+
+    @Test
+    public void test(){
+        Assert.assertEquals(1,mySqrt(1));
+        Assert.assertEquals(2,mySqrt(4));
+        Assert.assertEquals(2,mySqrt(8));
+    }
+
+    /**
+     *  二分法
+     * */
+    public int mySqrt(int x) {
+        long left=0;
+        long right=x/2+1;
+        while (left<right){
+            long mid=(left+right+1)>>>1;
+            if(mid*mid>x){
+                right=mid-1;
+            }else{
+                left=mid;
+            }
+        }
+        return (int)left;
+    }
+}
