eba0b72f2b0f7a0a42ef585850473d32cf7fb10e
qinge <qinge@tongbanjie.com>
2020-01-08 11:15:51 +0800

    200. 岛屿数量

--- src/main/java/com/ivan/leetcode/NumberOfIslands.java
+++ src/main/java/com/ivan/leetcode/NumberOfIslands.java
@@ -31,11 +31,126 @@ public class NumberOfIslands {
 
     @Test
     public void test(){
-        Assert.assertEquals(1,numIslands(new char[][]{{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}}));
-        Assert.assertEquals(3,numIslands(new char[][]{{'1','1','0','0','0'},{'1','1','0','0','0'},{'0','0','1','0','0'},{'0','0','0','1','1'}}));
+        Assert.assertEquals(1,numIslands(new char[][]{
+                                                                {'1','1','1','1','0'}
+                                                                ,{'1','1','0','1','0'}
+                                                                ,{'1','1','0','0','0'}
+                                                                ,{'0','0','0','0','0'}}));
+        Assert.assertEquals(3,numIslands(new char[][]{{'1','1','0','0','0'}
+                                                                ,{'1','1','0','0','0'}
+                                                                ,{'0','0','1','0','0'}
+                                                                ,{'0','0','0','1','1'}}));
+    }
+
+    class UnionFind {
+        int count; // # of connected components
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) { // for problem 200
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) { // path compression
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) { // union with rank
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx; rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
     }
 
     public int numIslands(char[][] grid) {
-        return 0;
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        UnionFind uf = new UnionFind(grid);
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r-1][c] == '1') {
+                        uf.union(r * nc + c, (r-1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r+1][c] == '1') {
+                        uf.union(r * nc + c, (r+1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c-1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c+1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
+                }
+            }
+        }
+
+        return uf.getCount();
     }
+
+    //深度优先法
+
+//    public void findIsland(char[][] grid,int i,int j){
+//        if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0'){
+//            return;
+//        }
+//        grid[i][j]='0';
+//        findIsland(grid,i-1,j);
+//        findIsland(grid,i+1,j);
+//        findIsland(grid,i,j-1);
+//        findIsland(grid,i,j+1);
+//    }
+//
+//    public int numIslands(char[][] grid) {
+//        if(grid==null || grid.length==0){
+//            return 0;
+//        }
+//        int count=0;
+//        for(int i=0;i<grid.length;i++){
+//            for(int j=0;j<grid[0].length;j++){
+//                if(grid[i][j]=='1'){
+//                    count++;
+//                    findIsland(grid,i,j);
+//                }
+//            }
+//        }
+//        return count;
+//    }
+
+
+
+
 }
