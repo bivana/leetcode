9d2720300fd4ce487d001e8a0550d79df5ddeecb
qinge <qinge@tongbanjie.com>
2020-04-16 16:45:16 +0800

    面试题40. 最小的k个数

--- src/main/java/com/ivan/leetcode/mianshiti/ZuiXiaoDeKgeShuLcof.java
+++ src/main/java/com/ivan/leetcode/mianshiti/ZuiXiaoDeKgeShuLcof.java
@@ -1,9 +1,12 @@
 package com.ivan.leetcode.mianshiti;
 
+import com.ivan.leetcode.util.ShowUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * 面试题40. 最小的k个数
@@ -38,19 +41,83 @@ public class ZuiXiaoDeKgeShuLcof {
     }
 
     public int[] getLeastNumbers(int[] arr, int k) {
-        int[] ans=new int[k];
-        Arrays.fill(ans,Integer.MAX_VALUE);
-        for(int v:arr){
-            for(int i=0;i<ans.length;i++){
-                if(v<ans[i]){
-                    for(int j=ans.length-2;j>=i;j--){
-                        ans[j+1]=ans[j];
-                    }
-                    ans[i]=v;
-                    break;
-                }
+        if(arr==null||arr.length==0){
+            return new int[]{};
+        }
+        quickFind(arr,0,arr.length-1,k);
+        int[] res=new int[k];
+        System.arraycopy(arr,0,res,0,k);
+        return res;
+    }
+
+    public void quickFind(int[] arr,int start,int end,int k){
+        if(start>=end){
+            return;
+        }
+        int pivot=partition(arr,start,end);
+        if(pivot==k){
+            return;
+        }else if(pivot<k){
+            quickFind(arr,pivot+1,end,k);
+        }else{
+            quickFind(arr,start,pivot-1,k);
+        }
+
+    }
+
+    public int partition(int[] arr,int start,int end){
+        int pivot=arr[start];
+        int left=start;
+        int right=end;
+        while (left!=right){
+            while (right>left && arr[right]>pivot){
+                right--;
+            }
+            while (left<right && arr[left]<=pivot ){
+                left++;
+            }
+            if(left<right){
+                int temp=arr[left];
+                arr[left]=arr[right];
+                arr[right]=temp;
             }
         }
-        return ans;
+        int p = arr[left];
+        arr[left] = arr[start];
+        arr[start] = p;
+        return left;
+    }
+
+
+
+
+    /**
+     * 堆
+     * */
+    public int[] getLeastNumbers2(int[] arr, int k) {
+        if (k == 0) {
+            return new int[0];
+        }
+        // 使用一个最大堆（大顶堆）
+        // Java 的 PriorityQueue 默认是小顶堆，添加 comparator 参数使其变成最大堆
+        Queue<Integer> heap = new PriorityQueue<>(k, (i1, i2) -> Integer.compare(i2, i1));
+
+        for (int e : arr) {
+            // 当前数字小于堆顶元素才会入堆
+            if (heap.isEmpty() || heap.size() < k || e < heap.peek()) {
+                heap.offer(e);
+            }
+            if (heap.size() > k) {
+                heap.poll(); // 删除堆顶最大元素
+            }
+        }
+
+        // 将堆中的元素存入数组
+        int[] res = new int[heap.size()];
+        int j = 0;
+        for (int e : heap) {
+            res[j++] = e;
+        }
+        return res;
     }
 }
