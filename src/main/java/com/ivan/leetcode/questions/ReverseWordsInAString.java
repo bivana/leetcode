ab701ff9377a47d8ec05562f3c9c588fe5738a20
qinge <qinge@tongbanjie.com>
2020-04-20 16:51:27 +0800

    151. 翻转字符串里的单词

---
+++ src/main/java/com/ivan/leetcode/questions/ReverseWordsInAString.java
@@ -0,0 +1,67 @@
+package com.ivan.leetcode.questions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 151. 翻转字符串里的单词
+ * 给定一个字符串，逐个翻转字符串中的每个单词。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * 示例 2：
+ *
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 示例 3：
+ *
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ *
+ *
+ * 说明：
+ *
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ *
+ *
+ * 进阶：
+ *
+ * 请选用 C 语言的用户尝试使用 O(1) 额外空间复杂度的原地解法。
+ * */
+public class ReverseWordsInAString {
+
+    @Test
+    public void test(){
+        Assert.assertEquals("blue is sky the",reverseWords("the sky is blue"));
+        Assert.assertEquals("world! hello",reverseWords("  hello world!  "));
+        Assert.assertEquals("example good a",reverseWords("a good   example"));
+    }
+
+    public String reverseWords(String s) {
+        if(s==null||"".equals(s)){
+            return "";
+        }
+        StringBuilder sb=new StringBuilder();
+        for(String str:s.split(" ")){
+            if(!str.trim().equals("")){
+                if(sb.length()==0){
+                    sb.insert(0,str);
+                }else{
+                    sb.insert(0,str+" ");
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
