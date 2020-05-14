c7df67f21b7481d86183af67f350189007157076
qinge <qinge@tongbanjie.com>
2020-05-14 10:33:17 +0800

    236. 二叉树的最近公共祖先

--- src/main/java/com/ivan/leetcode/TreeNode.java
+++ src/main/java/com/ivan/leetcode/TreeNode.java
@@ -8,4 +8,31 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
     public TreeNode(int x) { val = x; }
+
+    public static TreeNode create(Integer[] nums){
+        if(nums==null||nums.length==0){
+            return null;
+        }
+        TreeNode[] treeNodes=new TreeNode[nums.length];
+        treeNodes[0]=new TreeNode(nums[0]);
+        if(nums.length>1){
+            for(int i=1;i<nums.length;i++){
+                if(nums[i]==null){
+                    treeNodes[i]=null;
+                }else{
+                    treeNodes[i]=new TreeNode(nums[i]);
+                }
+                int parent=(i-1)/2;
+                if(treeNodes[parent]!=null){
+                    if((i&1)==1){
+                        treeNodes[parent].left=treeNodes[i];
+                    }else{
+                        treeNodes[parent].right=treeNodes[i];
+                    }
+                }
+
+            }
+        }
+        return treeNodes[0];
+    }
 }
