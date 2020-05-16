6140c00b902dafe926da8206f6840201a9a462aa
qinge <qinge@tongbanjie.com>
2020-05-16 10:17:50 +0800

    560. 和为K的子数组

---
+++ src/main/java/com/ivan/leetcode/questions/SubarraySumEqualsK.java
@@ -0,0 +1,73 @@
+package com.ivan.leetcode.questions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 560. 和为K的子数组
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ *
+ * 示例 1 :
+ *
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ * 说明 :
+ *
+ * 数组的长度为 [1, 20,000]。
+ * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+ * 通过次数47,956提交次数108,373
+ * 在真实的面试中遇到过这道题？
+ * */
+public class SubarraySumEqualsK {
+
+    @Test
+    public void test(){
+        Assert.assertEquals(2,subarraySum(new int[]{1,1,1},2));
+    }
+
+    //前缀和解法
+    public int subarraySum(int[] nums, int k) {
+        if(nums==null||nums.length==0){
+            return 0;
+
+        }
+        int ans=0;
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-k)){
+                ans+=map.get(sum-k);
+            }
+            if(map.containsKey(sum)){
+                map.put(sum,map.get(sum)+1);
+            }else{
+                map.put(sum,1);
+            }
+        }
+        return ans;
+    }
+
+//    public int subarraySum(int[] nums, int k) {
+//        if(nums==null||nums.length==0){
+//            return 0;
+//
+//        }
+//        int ans=0;
+//        int sum=0;
+//        for(int i=0;i<nums.length;i++){
+//            sum=0;
+//            for(int j=i;j<nums.length;j++){
+//                sum+=nums[j];
+//                if(sum==k){
+//                    ans++;
+//                }
+//            }
+//        }
+//        return ans;
+//    }
+}
