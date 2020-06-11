17ad2b6b949cd49c6d01d86e18bc7256464b8696
bivana <502331290@qq.com>
2020-06-12 07:14:01 +0800

    15. 三数之和

--- src/main/java/com/ivan/leetcode/questions/ThreeSum.java
+++ src/main/java/com/ivan/leetcode/questions/ThreeSum.java
@@ -1,5 +1,8 @@
 package com.ivan.leetcode.questions;
 
+import com.ivan.leetcode.util.ShowUtil;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,44 +24,78 @@ import java.util.List;
  * */
 public class ThreeSum {
 
-    public static void main(String[] args){
-        ThreeSum threeSum=new ThreeSum();
-        List<List<Integer>> list=threeSum.threeSum(new int[]{-2,0,0,2,2});
-        for(List<Integer> l:list){
-            for(Integer a:l){
-                System.out.print(a);
-            }
-            System.out.println();
-        }
+    @Test
+    public void test(){
+        List<List<Integer>> rs=threeSum(new int[]{-2,0,0,2,2});
+        ShowUtil.showListMatrix(rs);
     }
 
-
-
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<List<Integer>>();
+        List<List<Integer>> result=new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i<nums.length-2;i++){
-            int l=i+1;
-            int r=nums.length-1;
+            int start=i+1;
+            int end=nums.length-1;
             if(nums[i]>0){
-                return result;
+                break;
             }else if(i>0 && nums[i]==nums[i-1]){
                 continue;
-            }else {
-                while (l<r){
-                    int temp=nums[i]+nums[l]+nums[r];
+            }else{
+                while (start<end){
+                    int temp=nums[i]+nums[start]+nums[end];
                     if(temp==0){
-                        result.add(Arrays.asList(new Integer[]{nums[i],nums[l],nums[r]}));
-                        while(l < r && nums[l] == nums[++l]);
-                        while(l < r && nums[r] == nums[--r]);
-                    }else if(temp>0){
-                        while(l < r && nums[r] == nums[--r]);
+                        result.add(Arrays.asList(new Integer[]{nums[i],nums[start],nums[end]}));
+                        start++;
+                        while (start<end && nums[start]==nums[start-1]){
+                            start++;
+                        }
+                        end--;
+                        while (start<end && nums[end]==nums[end+1]){
+                            end--;
+                        }
+                    }else if (temp<0){
+                        start++;
+                        while (start<end && nums[start]==nums[start-1]){
+                            start++;
+                        }
                     }else{
-                        while(l < r && nums[l] == nums[++l]);
+                        end--;
+                        while (start<end && nums[end]==nums[end+1]){
+                            end--;
+                        }
                     }
                 }
             }
         }
         return result;
     }
+
+
+//    public List<List<Integer>> threeSum(int[] nums) {
+//        List<List<Integer>> result=new ArrayList<List<Integer>>();
+//        Arrays.sort(nums);
+//        for(int i=0;i<nums.length-2;i++){
+//            int l=i+1;
+//            int r=nums.length-1;
+//            if(nums[i]>0){
+//                return result;
+//            }else if(i>0 && nums[i]==nums[i-1]){
+//                continue;
+//            }else {
+//                while (l<r){
+//                    int temp=nums[i]+nums[l]+nums[r];
+//                    if(temp==0){
+//                        result.add(Arrays.asList(new Integer[]{nums[i],nums[l],nums[r]}));
+//                        while(l < r && nums[l] == nums[++l]);
+//                        while(l < r && nums[r] == nums[--r]);
+//                    }else if(temp>0){
+//                        while(l < r && nums[r] == nums[--r]);
+//                    }else{
+//                        while(l < r && nums[l] == nums[++l]);
+//                    }
+//                }
+//            }
+//        }
+//        return result;
+//    }
 }
